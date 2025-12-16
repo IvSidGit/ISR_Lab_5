9b92528058238a1ffdeffd4c73dea55c903066f6
IvSidGit <ivsid05@mail.ru>
2025-12-16 14:36:13 +0300

    Add method "sum" in Main file

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
-class Main{
+class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         printHelloWorld();
+        System.out.println(1,2);
     }
-    public static void printHelloWorld(){
+
+    public static void printHelloWorld() {
         System.out.println("Hello, World!");
     }
+
+    public static void sum(int a, int b) {
+        return a + b;
+    }
+
 }
