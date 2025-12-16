89bcaaade0080f19f0e3a04df6b21d6ddb9ffa59
IvSidGit <ivsid05@mail.ru>
2025-12-16 14:35:13 +0300

    Add method "printHelloWorld" in Main file

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
+        printHelloWorld();
+    }
+    public static void printHelloWorld(){
+        System.out.println("Hello, World!");
     }
 }
