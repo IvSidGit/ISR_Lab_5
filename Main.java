3347a7383bb742638ee86831e3ebab539070c2e2
IvSidGit <ivsid05@mail.ru>
2025-12-16 15:19:51 +0300

    Added initialization array 'arr'

--- Main.java
+++ Main.java
@@ -3,6 +3,9 @@ class Main{
         System.out.print("Это добавлено в ветке dev");
 
         int[] arr = new int[];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
     }
 
     public static printArr(int[] arr){
